73710a212ca762e8bc9c716abc93b0a29d464e5e
VICTOR HUGO <vargas2000@gmail.com>
2020-07-24 15:52:52 -0500

    Se introduce matriz para simplificar la asigancion aleatoria de los duclces 24-07-2020 03:52pm

--- js/app.js
+++ js/app.js
@@ -22,13 +22,49 @@ function flastit ()   {clrtitwh (),clrtitbl ()};
 
 
 // □□□□□□  2 Modulo Generación de numero aleatorio de 1 a 4 y asignación a elementos□□□□□□
+
+
 function asigna(){
-var cola = ["a1","a2","a3","a4","a5","a6","a7"] ;
 var i ;
-var numa ="";
+var numa = "";
+var cola = [a1,a2,a3,a4,a5,a6,a7] ;
 for (i=0; i < cola.length ;i++)
 { numa += (cola[i]=Math.floor((Math.random() * 4) + 1))};
-alert(numa);
+var a1=cola.slice(0,1)
+if (a1==1){$("#a1").attr("src","image/1.png");}
+if (a1==2){$("#a1").attr("src","image/2.png");}
+if (a1==3){$("#a1").attr("src","image/3.png");}
+if (a1==4){$("#a1").attr("src","image/4.png");}
+var a2=cola.slice(1,2)
+if (a2==1){$("#a2").attr("src","image/1.png");}
+if (a2==2){$("#a2").attr("src","image/2.png");}
+if (a2==3){$("#a2").attr("src","image/3.png");}
+if (a2==4){$("#a2").attr("src","image/4.png");}
+var a3=cola.slice(2,3)
+if (a3==1){$("#a3").attr("src","image/1.png");}
+if (a3==2){$("#a3").attr("src","image/2.png");}
+if (a3==3){$("#a3").attr("src","image/3.png");}
+if (a3==4){$("#a3").attr("src","image/4.png");}
+var a4=cola.slice(3,4)
+if (a4==1){$("#a4").attr("src","image/1.png");}
+if (a4==2){$("#a4").attr("src","image/2.png");}
+if (a4==3){$("#a4").attr("src","image/3.png");}
+if (a4==4){$("#a4").attr("src","image/4.png");}
+var a5=cola.slice(4,5)
+if (a5==1){$("#a5").attr("src","image/1.png");}
+if (a5==2){$("#a5").attr("src","image/2.png");}
+if (a5==3){$("#a5").attr("src","image/3.png");}
+if (a5==4){$("#a5").attr("src","image/4.png");}
+var a6=cola.slice(5,6)
+if (a6==1){$("#a6").attr("src","image/1.png");}
+if (a6==2){$("#a6").attr("src","image/2.png");}
+if (a6==3){$("#a6").attr("src","image/3.png");}
+if (a6==4){$("#a6").attr("src","image/4.png");}
+var a7=cola.slice(6,7)
+if (a7==1){$("#a7").attr("src","image/1.png");}
+if (a7==2){$("#a7").attr("src","image/2.png");}
+if (a7==3){$("#a7").attr("src","image/3.png");}
+if (a7==4){$("#a7").attr("src","image/4.png");}
 }
 
 function asignb(){
@@ -37,16 +73,15 @@ var numb = "";
 var colb = [b1,b2,b3,b4,b5,b6,b7] ;
 for (i=0; i < colb.length ;i++)
 { numb += (colb[i]=Math.floor((Math.random() * 4) + 1))};
-alert(numb);
 var b1=colb.slice(1,2)
-if (b1==4){alert(b1);}
-
-}
-function convertir (){}
+if (b1==1){$("#b1").attr("src","image/1.png");}
+if (b1==2){$("#b1").attr("src","image/2.png");}
+if (b1==3){$("#b1").attr("src","image/3.png");}
+if (b1==4){$("#b1").attr("src","image/4.png");}}
 // □□□□□□  Fin M2 □□□□□□
 
 // □□□□□□  3 Modulo Actividad del boton Inicar □□□□□□
-$(".btn-reinicio").click(function(){ $(".btn-reinicio").css(asignb(),convertir())})
+$(".btn-reinicio").click(function(){ $(".btn-reinicio").css(asigna(),asignb())})
 
 
 
